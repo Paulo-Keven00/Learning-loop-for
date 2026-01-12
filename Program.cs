6ca9c01d3dfc40db7fccb484bb018a9aa129e12c
Paulo Keven <paulokevenm2@gmail.com>
2026-01-12 01:45:07 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿namespace LAÇOS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Licao6();
+    }
+    static void Licao1()
+    {
+        //CONTAGEM BASICA:
+        //Crie um for que imprima os numeros de 0 a 9.
+
+        for(int n=0;n <= 9; n++)
+        {
+            Console.Write(n+", ");
+        }
+        Console.Write("\n");
+    }
+    static void Licao2()
+    {
+       //Contagem regressiva:
+       //Crie um for que imprima os números de 15 ate 1.
+
+       for(int n=15;n >= 1; n--)
+        {
+            Console.Write(n+ ", ");
+        }
+        Console.Write("\n"); 
+    } 
+    static void Licao3()
+    {
+        //Numeros pares:
+        //Imprima apenas os números pares de 0 a 30.
+
+        for(int n=0;n <= 30; n +=2)
+        {
+           Console.Write(n+ ", "); 
+        }
+        Console.Write("\n");
+    }
+    static void Licao4()
+    {
+       //Sem executar o código, responda: quantas vezes o laço será executado?
+       //for (int i = 2; i <= 12; i += 2)
+       //Resposta = 6 repetiçoes
+    }
+    static void Licao5()
+    {
+        //Dado o array abaixo, imprima todos os valores utilizando for:
+        int [] valores = {4, 9, 15, 2, 7};
+        
+        for(int i=0;i < 5; i++)
+        {
+            Console.Write($"{valores[i]}, ");
+        }
+    }
+    static void Licao6()
+    {
+        //Soma controlada:
+        //Crie um for que percorra os números de 1 a 20 e exiba a soma total ao final.
+        int [] valores = {4, 9, 15, 2, 7};
+
+        for(int i=0;i < 5; i++)
+        {
+           if(valores[i] > 5)
+            {
+                Console.Write($"{valores[i]},");
+            } 
+        }
+    }
+    
+}
